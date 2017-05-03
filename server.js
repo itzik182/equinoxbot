f84c868760678cf419d624440928c36209d3707b
System <none>
2017-05-03 10:01:19 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -55,11 +55,24 @@ bot.on('message', (payload, reply) => {
           
           bot.sendMessage(senderId, 
             { 
-              text : article.title,
               attachment : {
                 type : 'template',
                 payload : {
-                  template_type : 'generic'
+                  template_type : 'generic',
+                  elements : [
+                    {
+                      title : article.title,
+                      // image_url : article.urlToImage,
+                      // subtitle : article.description,
+                      // default_action : {
+                      //   type : 'web_url',
+                      //   url : article.url,
+                      //   messenger_extensions : true,
+                      //   webview_height_ratio : 'tall',
+                      //   fallback_url : 'https://news.ycombinator.com/'
+                      // }
+                    }
+                  ]
                 }
               }
             }
