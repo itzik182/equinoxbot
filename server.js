27c32325c3264b89d3af4cf8078767d6c6ffe169
System <none>
2017-05-03 08:50:33 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -5,13 +5,13 @@
 var Bot = require('messenger-bot')
 var http = require('http')
 
-const users = {}
-
 const bot = new Bot({
   token: process.env.PAGE_TOKEN,
   verify: process.env.VERIFY_TOKEN,
   app_secret: process.env.APP_SECRET
 })
+
+const users = {}
 
 bot.on('postback', (payload) => {
   if (payload.postback.payload === 'GET_STARTED_PAYLOAD') {
@@ -22,20 +22,18 @@ bot.on('postback', (payload) => {
         console.log(error)
       }
       
-      console.log(profile)
-      
       users[senderId] = {
         pageScopedId : senderId,
         firstName : profile.first_name, 
         lastName : profile.last_name,
       }
       
-      console.log(users)
+      bot.sendMessage(senderId, {text : `Thanks ${profile.first_name}, you've been added to our userbase!`})
+      console.log(`${profile.first_name} ${profile.last_name} added to users.`)
+      
     })
   }
 })
-
-
 
 http.createServer(bot.middleware()).listen(3000)
 console.log('Echo bot server running at port 3000.')
