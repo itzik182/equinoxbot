d2bf94899bded283a62cba7f255e228696ae4d44
System <none>
2017-05-03 09:00:39 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -12,28 +12,28 @@ const bot = new Bot({
 })
 
 const users = {}
-
 bot.on('postback', (payload) => {
   if (payload.postback.payload === 'GET_STARTED_PAYLOAD') {
     const senderId = payload.sender.id
-    
+ 
     bot.getProfile(senderId, (error, profile) => {
       if (error) {
         console.log(error)
       }
-      
+ 
       users[senderId] = {
         pageScopedId : senderId,
         firstName : profile.first_name, 
         lastName : profile.last_name,
       }
-      
-      bot.sendMessage(senderId, {text : `Thanks ${profile.first_name}, you've been added to our userbase!`})
-      console.log(`${profile.first_name} ${profile.last_name} added to users.`)
-      
-    })
+ 
+      bot.sendMessage(
+        senderId, 
+        {text : `Thanks ${profile.first_name},  you're now registered!`}
+      )
+      console.log(`${profile.first_name} ${profile.last_name} => users`)
+   })
   }
 })
-
 http.createServer(bot.middleware()).listen(3000)
 console.log('Echo bot server running at port 3000.')
